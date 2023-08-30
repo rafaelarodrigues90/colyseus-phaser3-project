a810277f24caa8b4c359ade3ba46e108d4a2301a
Rafaela Rodrigues <ltcmdrdata@protonmail.com>
2023-08-30 12:05:32 -0300

    fix: broken tutorial code

--- client/index.ts
+++ client/index.ts
@@ -2,21 +2,30 @@ import { Client, Room } from "colyseus.js";
 import Phaser from "phaser";
 
 export class GameScene extends Phaser.Scene {
-  client = new Client("ws://localhost:2567");
   room: Room;
+
+  currentPlayer: Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
   playerEntities: { [sessionId: string]: any } = {};
+
+  debugFPS: Phaser.GameObjects.Text;
+
+  localRef: Phaser.GameObjects.Rectangle;
+  remoteRef: Phaser.GameObjects.Rectangle;
+
+  cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
 
   inputPayload = {
     left: false,
     right: false,
     up: false,
     down: false,
+    tick: undefined,
   };
 
-  cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
+  elapsedTime = 0;
+  fixedTimeStep = 1000 / 60;
 
-  currentPlayer: Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
-  remoteRef: Phaser.GameObjects.Rectangle;
+  currentTick: number = 0;
 
   preload() {
     this.load.image(
@@ -27,64 +36,104 @@ export class GameScene extends Phaser.Scene {
     this.cursorKeys = this.input.keyboard.createCursorKeys();
   }
 
-  async create() {
+  async connect() {
+    // add connection status text
+    const connectionStatusText = this.add
+      .text(0, 0, "Trying to connect with the server...")
+      .setStyle({ color: "#ff0000" })
+      .setPadding(4);
+
+    const client = new Client("ws://localhost:2567");
+
     try {
-      this.room = await this.client.joinOrCreate("my_room");
+      this.room = await client.joinOrCreate("my_room", {});
 
-      this.room.state.players.onAdd((player, sessionId) => {
-        const entity = this.physics.add.image(player.x, player.y, "ship_0001");
-        this.playerEntities[sessionId] = entity;
-
-        if (sessionId === this.room.sessionId) {
-          this.currentPlayer = entity;
-          this.remoteRef = this.add.rectangle(
-            0,
-            0,
-            entity.width,
-            entity.height
-          );
-          this.remoteRef.setStrokeStyle(1, 0xff0000);
-
-          player.onChange(() => {
-            this.remoteRef.x = player.x;
-            this.remoteRef.y = player.y;
-          });
-        }
-
-        player.onChange(() => {
-          entity.setData("serverX", player.x);
-          entity.setData("serverY", player.y);
-        });
-      });
-
-      this.room.state.players.onRemove((player, sessionId) => {
-        const entity = this.playerEntities[sessionId];
-        if (entity) {
-          entity.destroy();
-
-          delete this.playerEntities[sessionId];
-        }
-      });
+      // connection successful!
+      connectionStatusText.destroy();
     } catch (e) {
-      console.error(e);
+      // couldn't connect
+      connectionStatusText.text = "Could not connect with the server.";
     }
   }
 
-  update(time: number, delta: number): void {
-    // game loop
-    if (!this.room) {
-      return;
-    }
+  async create() {
+    this.cursorKeys = this.input.keyboard.createCursorKeys();
+    this.debugFPS = this.add.text(4, 4, "", { color: "#ff0000" });
 
+    await this.connect();
+
+    this.room.state.players.onAdd((player, sessionId) => {
+      const entity = this.physics.add.image(player.x, player.y, "ship_0001");
+      this.playerEntities[sessionId] = entity;
+
+      // is current player
+      if (sessionId === this.room.sessionId) {
+        this.currentPlayer = entity;
+
+        this.localRef = this.add.rectangle(0, 0, entity.width, entity.height);
+        this.localRef.setStrokeStyle(1, 0x00ff00);
+
+        this.remoteRef = this.add.rectangle(0, 0, entity.width, entity.height);
+        this.remoteRef.setStrokeStyle(1, 0xff0000);
+
+        player.onChange(() => {
+          this.remoteRef.x = player.x;
+          this.remoteRef.y = player.y;
+        });
+      } else {
+        // listening for server updates
+        player.onChange(() => {
+          //
+          // we're going to LERP the positions during the render loop.
+          //
+          entity.setData("serverX", player.x);
+          entity.setData("serverY", player.y);
+        });
+      }
+    });
+
+    // remove local reference when entity is removed from the server
+    this.room.state.players.onRemove((player, sessionId) => {
+      const entity = this.playerEntities[sessionId];
+      if (entity) {
+        entity.destroy();
+        delete this.playerEntities[sessionId];
+      }
+    });
+
+    // this.cameras.main.startFollow(this.ship, true, 0.2, 0.2);
+    // this.cameras.main.setZoom(1);
+    this.cameras.main.setBounds(0, 0, 800, 600);
+  }
+
+  update(time: number, delta: number): void {
+    // skip loop if not connected yet.
     if (!this.currentPlayer) {
       return;
     }
+
+    this.elapsedTime += delta;
+    while (this.elapsedTime >= this.fixedTimeStep) {
+      this.elapsedTime -= this.fixedTimeStep;
+      this.fixedTick(time, this.fixedTimeStep);
+    }
+
+    this.debugFPS.text = `Frame rate: ${this.game.loop.actualFps}`;
+  }
+
+  fixedTick(time, delta) {
+    this.currentTick++;
+
+    // const currentPlayerRemote = this.room.state.players.get(this.room.sessionId);
+    // const ticksBehind = this.currentTick - currentPlayerRemote.tick;
+    // console.log({ ticksBehind });
 
     const velocity = 2;
     this.inputPayload.left = this.cursorKeys.left.isDown;
     this.inputPayload.right = this.cursorKeys.right.isDown;
     this.inputPayload.up = this.cursorKeys.up.isDown;
     this.inputPayload.down = this.cursorKeys.down.isDown;
+    this.inputPayload.tick = this.currentTick;
     this.room.send(0, this.inputPayload);
 
     if (this.inputPayload.left) {
@@ -99,7 +148,12 @@ export class GameScene extends Phaser.Scene {
       this.currentPlayer.y += velocity;
     }
 
+    this.localRef.x = this.currentPlayer.x;
+    this.localRef.y = this.currentPlayer.y;
+
     for (let sessionId in this.playerEntities) {
+      // interpolate all player entities
+      // (except the current player)
       if (sessionId === this.room.sessionId) {
         continue;
       }
@@ -109,10 +163,6 @@ export class GameScene extends Phaser.Scene {
 
       entity.x = Phaser.Math.Linear(entity.x, serverX, 0.2);
       entity.y = Phaser.Math.Linear(entity.y, serverY, 0.2);
-    }
-
-    if (!this.currentPlayer) {
-      return;
     }
   }
 }
--- server/src/rooms/MyRoom.ts
+++ server/src/rooms/MyRoom.ts
@@ -1,50 +1,74 @@
 import { Room, Client } from "@colyseus/core";
-import { MyRoomState, Player } from "./schema/MyRoomState";
+import { InputData, MyRoomState, Player } from "./schema/MyRoomState";
 
 export class MyRoom extends Room<MyRoomState> {
   maxClients = 20;
 
+  fixedTimeStep = 1000 / 60;
+
   onCreate(options: any) {
     this.setState(new MyRoomState());
 
+    // set map dimensions
+    this.state.mapWidth = 800;
+    this.state.mapHeight = 600;
+
     this.onMessage(0, (client, input) => {
+      // handle player input
       const player = this.state.players.get(client.sessionId);
-      const velocity = 2;
 
-      if (input.left) {
-        player.x -= velocity;
-      } else if (input.right) {
-        player.x += velocity;
+      // enqueue input to user input buffer.
+      player.inputQueue.push(input);
+    });
+
+    let elapsedTime = 0;
+    this.setSimulationInterval((deltaTime) => {
+      elapsedTime += deltaTime;
+
+      while (elapsedTime >= this.fixedTimeStep) {
+        elapsedTime -= this.fixedTimeStep;
+        this.fixedTick(this.fixedTimeStep);
       }
+    });
+  }
 
-      if (input.up) {
-        player.y -= velocity;
-      } else if (input.down) {
-        player.y += velocity;
+  fixedTick(timeStep: number) {
+    const velocity = 2;
+
+    this.state.players.forEach((player) => {
+      let input: InputData;
+
+      // dequeue player inputs
+      while ((input = player.inputQueue.shift())) {
+        if (input.left) {
+          player.x -= velocity;
+        } else if (input.right) {
+          player.x += velocity;
+        }
+
+        if (input.up) {
+          player.y -= velocity;
+        } else if (input.down) {
+          player.y += velocity;
+        }
+
+        player.tick = input.tick;
       }
     });
   }
 
   onJoin(client: Client, options: any) {
-    console.debug(client.sessionId, "joined!");
-
-    const mapWidth = 800;
-    const mapHeight = 600;
+    console.log(client.sessionId, "joined!");
 
     const player = new Player();
+    player.x = Math.random() * this.state.mapWidth;
+    player.y = Math.random() * this.state.mapHeight;
 
-    // place Player at a random position
-    player.x = Math.random() * mapWidth;
-    player.y = Math.random() * mapHeight;
-
-    // place player in the map of players by its sessionId
-    // (client.sessionId is unique per connection!)
     this.state.players.set(client.sessionId, player);
   }
 
   onLeave(client: Client, consented: boolean) {
-    console.debug(client.sessionId, "left!");
-
+    console.log(client.sessionId, "left!");
     this.state.players.delete(client.sessionId);
   }
 
